b0e06f74d6dadff3b0673b5f35811a14f6a7406b
DavePaurillo <paurillo.dave@gmail.com>
2026-05-08 20:10:18 +0800

    nuke the project bc of AI slop

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import env from "./env.js";
-import app from "./server.js";
-
-app.listen(env.PORT, () => {
-  console.log(
-    `🚀 Server running at http://${env.HOST}:${env.PORT} in ${env.NODE_ENV} mode`,
-  );
-});
